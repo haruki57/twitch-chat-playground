379f0368742fab96bfef69a2f7b99a33b8b06c0d
Haruki <haruki@example.com>
2021-12-31 19:58:12 +0900

    Add type of context by myself

--- src/bot.ts
+++ src/bot.ts
@@ -22,7 +22,7 @@ export const init = () => {
   client.connect();
 
   // Called every time a message comes in
-  function onMessageHandler(target: any, context: any, msg: string, self: any) {
+  function onMessageHandler(target: string, context: Context, msg: string, self: any) {
     if (self) { return; } // Ignore messages from the bot
 
     // Remove whitespace from chat message
@@ -49,4 +49,38 @@ export const init = () => {
     console.log(`* Connected to ${addr}:${port}`);
     (document.querySelector('#isConnected') as HTMLElement).innerText = 'Connected!';
   }
+}
+interface Context {
+  'badge-info': BadgeInfo;
+  badges: Badges;
+  'client-nonce': string;
+  color: string;
+  'display-name': string;
+  emotes: Emotes;
+  'first-msg': boolean;
+  'flags'?: null;
+  id: string;
+  mod: boolean;
+  'room-id': string;
+  subscriber: boolean;
+  'tmi-sent-ts': string;
+  turbo: boolean;
+  'user-id': string;
+  'user-type'?: null;
+  'emotes-raw': string;
+  'badge-info-raw': string;
+  'badges-raw': string;
+  username: string;
+  'message-type': string;
+}
+interface BadgeInfo {
+  subscriber: string;
+}
+interface Badges {
+  broadcaster: string;
+  subscriber: string;
+  premium: string;
+}
+interface Emotes {
+  [stampId: string]: string[];
 }
